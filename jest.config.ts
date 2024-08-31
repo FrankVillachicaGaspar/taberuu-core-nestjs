3cbdcc77dbaf7672bb503721272201c382f106b2
Frank Villachica Gaspar <frankvillachica@outlook.com>
2024-08-31 01:54:17 -0500

    🔧 (workflow-config): adding a workflow for testing and build

--- jest.config.ts
+++ jest.config.ts
@@ -32,10 +32,10 @@ const config: Config = {
   coverageReporters: ['text-summary', 'lcov', 'json'],
   coverageThreshold: {
     global: {
-      branches: 65,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
   coverageDirectory: './coverage',
